c411154a9611bee8123018be4d5fdebce89d2cc3
Fideborios <touringlxs@gmail.com>
2017-04-19 17:15:11 +0200

    New analysis

--- Karina.R
+++ Karina.R
@@ -1,51 +1,17 @@
-library(foreign, lib.loc = "c:/Program Files/R/R-3.3.2/library/")
-library(haven,lib.loc = "c:/Program Files/R/R-3.3.2/library/")
-library(mice,lib.loc = "c:/Program Files/R/R-3.3.2/library/")
-library(VIM,lib.loc = "c:/Program Files/R/R-3.3.2/library/")
-library(ggplot2,lib.loc = "c:/Program Files/R/R-3.3.2/library/")
-library(nnet,lib.loc = "c:/Program Files/R/R-3.3.2/library/")
+library(foreign)
+library(haven)
+library(mice)
+library(VIM)
+library(ggplot2)
+library(nnet)
 library(lme4)
-library(npmlt)
 
 
 
-KarinaDS <- read_sav("C:/Users/z047185/Desktop/Karina/NEW3 - exclusie - inclusief 2 pt GRainne 09122016.sav", user_na = F)
+
+KarinaDS <- read_sav("NEW3 - exclusie - inclusief 2 pt GRainne 09122016.sav", user_na = F)
 KarinaDS <- as.data.frame(KarinaDS)
 
-
-ggplot() +  ggtitle('Barplot of Number of Studies') + 
-  geom_bar(aes(y = ..count..,x = as.factor(No_study)),colour="red", fill="darkgreen",data=KarinaDS) +theme_light()
-
-ggplot() +  geom_bar(aes(y = ..count..,x = as.factor(Age_binair)),colour="red", fill="blue",data=KarinaDS)
-
-ggplot() +  geom_bar(aes(y = ..count..,x = as.factor(Gender)),colour="red", fill="blue",data=KarinaDS)
-
-ggplot() + geom_histogram(aes(x = Age),data=KarinaDS,colour = 'red',fill = 'blue') + theme_classic()
-
-ggplot() +  geom_histogram(aes(y = ..count..,x = RBC_before),colour="red", fill="blue",data=KarinaDS)
-
-
-ggplot() +  geom_bar(aes(y = ..count..,x = as.factor(RBCbefore_binairy)),colour="red", fill="blue",data=KarinaDS)
-
-
-
-
-k=1
-for(i in colnames(a)[which(as.integer(a[2,])<10)] ){
-  assign(paste("Plot", k,sep="") , eval(parse(text=paste("ggplot() +ggtitle('Barplot of",i," ') +   geom_bar(aes(y = ..count..,x = as.factor(", i ,")),colour='red', fill='darkgreen',data=KarinaDS) +theme_light()") )))
-  k=k+1
-}
-
-multiplot(Plot1, Plot2, Plot3, Plot4, cols=2)
-multiplot(Plot5, Plot6, Plot7, Plot8, cols=2)
-multiplot(Plot9, Plot10, Plot11, Plot12, cols=2)
-multiplot(Plot13, Plot14, Plot15, Plot16, cols=2)
-multiplot(Plot17, Plot18, Plot19, Plot20, cols=2)
-multiplot(Plot21, Plot22, Plot23, Plot24, cols=2)
-multiplot(Plot25, Plot26, Plot27, Plot28, cols=2)
-multiplot(Plot29, Plot30, Plot31, Plot32, cols=2)
-multiplot(Plot33, Plot34, Plot35, Plot36, cols=2)
-multiplot(Plot37, Plot38, Plot39, Plot40, cols=2)
 
 
 a= matrix(ncol = dim(KarinaDS)[2], nrow = 2, 
@@ -57,6 +23,28 @@ for(i in 1:dim(KarinaDS)[2]){
   a[2,i]= length(which(!is.na(unique(KarinaDS[,i]))))
   
 }
+
+k=1
+for(i in colnames(a)[which(as.integer(a[2,])<10)] ){
+  assign(paste("Plot", k,sep="") , eval(parse(text=paste("ggplot() +ggtitle('Barplot of",i," ') +   geom_bar(aes(y = ..count..,x = as.factor(", i ,")),colour='red', fill='darkgreen',data=KarinaDS) +theme_light()") )))
+  k=k+1
+}
+
+
+
+
+multiplot(Plot1, Plot2, Plot3, Plot4, cols=3)
+multiplot(Plot5, Plot6, Plot7, Plot8, cols=2)
+multiplot(Plot9, Plot10, Plot11, Plot12, cols=2)
+multiplot(Plot13, Plot14, Plot15, Plot16, cols=2)
+multiplot(Plot17, Plot18, Plot19, Plot20, cols=2)
+multiplot(Plot21, Plot22, Plot23, Plot24, cols=2)
+multiplot(Plot25, Plot26, Plot27, Plot28, cols=2)
+multiplot(Plot29, Plot30, Plot31, Plot32, cols=2)
+multiplot(Plot33, Plot34, Plot35, Plot36, cols=2)
+multiplot(Plot37, Plot38, Plot39, Plot40, cols=2)
+
+
 
 
 
